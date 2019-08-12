1f5d13a08b1c94622d84bf1fb6bfe51c349cce45
Omarfinge <omarfinge@gmail.com>
2019-08-12 12:15:57 +0200

    Working on server - First commit -- communication succes!

--- Server/src/server.js
+++ Server/src/server.js
@@ -8,11 +8,12 @@ var app = express();
 
 app.use(express.static(path.dirname(module.parent.id)));
 
-app.get('/:pageDir',function(req,res){
-	console.log((path.dirname(module.parent.id)));
-	var page = (path.dirname(module.parent.id)) + req.params.pageDir;
-	res.send('111111');
-	res.sendFile(page);
+app.get('/boo/:pageDir',function(req,res){
+	// console.log((path.dirname(module.parent.id)));
+	// var page = (path.dirname(module.parent.id)) + req.params.pageDir;
+	console.log(req.params.pageDir);
+	res.send('sauce biggy chef');
+	//res.sendFile(page);
 	/*
 	mariadb.createConnection({host: '127.0.0.1', user: 'root', password: '1cluedu2mariadb3', database: 'cluedu', port: '3306', multipleStatements: true})
 	.then(conn => {
